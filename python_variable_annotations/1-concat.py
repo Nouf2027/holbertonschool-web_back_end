1783a917e58d5b6fef4f7d46b42cf3a588905dd4
Nouf Almutairi <noufn8988@gmail.com>
2026-02-21 22:51:55 +0300

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+"""Module for concatenating two strings"""
 
 
-"""Basic annotations - add"""
-def concat( a: str , b: str ) -> str :
-  
-return a + b
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings
+
+    Args:
+        str1: First string
+        str2: Second string
+
+    Returns:
+        Concatenated string
+    """
+    return str1 + str2
