0b7766a88a8481f4a27f4b7008f15be26d53852b
Nouf Almutairi <noufn8988@gmail.com>
2026-02-21 22:38:49 +0300

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+"""Basic annotations - add"""
 
-
-def add(a: float, b: float) 
+def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
     return a + b
